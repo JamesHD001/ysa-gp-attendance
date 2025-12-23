4db9270711298d64df6d89587ff387ada00949f4
JamesHD001 <henry.jameshd@gmail.com>
2025-12-23 18:01:47 +0100

    update files

--- js/auth.js
+++ js/auth.js
@@ -22,6 +22,6 @@ function logout() {
 function protectPage() {
     const loggedIn = localStorage.getItem("isAdminLoggedIn");
     if (!loggedIn) {
-        window.location.href = "login.html";
+        window.location.href = "index.html";
     }
 }
